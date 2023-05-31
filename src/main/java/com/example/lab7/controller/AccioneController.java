7cbee3a634fffa1b8b85e0cad67560273408bace
r.b4rr10s <a20202073@pucp.edu.pe>
2023-05-31 04:11:29 -0500

    Guardar accion

--- src/main/java/com/example/lab7/controller/AccioneController.java
+++ src/main/java/com/example/lab7/controller/AccioneController.java
@@ -1,13 +1,42 @@
 package com.example.lab7.controller;
 
+import com.example.lab7.entities.Accione;
+import com.example.lab7.entities.Pago;
+import com.example.lab7.repository.AccioneRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value="/acciones", produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
 public class AccioneController {
-    
+    final AccioneRepository accioneRepository;
+
+    public AccioneController(AccioneRepository accioneRepository) {
+        this.accioneRepository = accioneRepository;
+    }
+
+
+    @PostMapping("/save")
+    public ResponseEntity<HashMap<String, Object>> registrarPago(@RequestBody Accione accione){
+        HashMap<String, Object> rspta = new HashMap<>();
+        accioneRepository.save(accione);
+        rspta.put("idCreado", accione.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(rspta);
+    }
+    //Exceptionhandlerpost
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<HashMap<String,String>> gestionException(HttpServletRequest request){
+        HashMap<String,String> responseMap = new HashMap<>();
+        if(request.getMethod().equals("POST")){
+            responseMap.put("Result", "Error");
+        }
+        return ResponseEntity.badRequest().body(responseMap);
+    }
 }
