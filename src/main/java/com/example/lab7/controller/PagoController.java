8541c6fabfdd83781281af7d812c15a67c10a7ca
r.b4rr10s <a20202073@pucp.edu.pe>
2023-05-31 15:30:13 -0500

    Validar ids

--- src/main/java/com/example/lab7/controller/PagoController.java
+++ src/main/java/com/example/lab7/controller/PagoController.java
@@ -1,6 +1,7 @@
 package com.example.lab7.controller;
 
 import com.example.lab7.entities.Pago;
+import com.example.lab7.entities.Usuario;
 import com.example.lab7.repository.PagoRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -9,8 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin
@@ -29,11 +32,31 @@ public class PagoController {
 
     @PostMapping("/registrarPago")
     public ResponseEntity<HashMap<String, Object>> registrarPago(@RequestBody Pago pago){
-        HashMap<String, Object> rspta = new HashMap<>();
-        pagoRepository.save(pago);
-        rspta.put("id", pago.getId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(rspta);
-    }
+        HashMap<String, Object> responseJson = new HashMap<>();
+        if(pago.getId()!=null && pago.getId()>0){
+            boolean flag = false;
+            for (Pago pago1 : pagoRepository.findAll()){
+                if(Objects.equals(pago1.getId(), pago.getId())){
+                    flag = true;
+                    break;
+                }
+
+            }
+
+            if(!flag){
+                pagoRepository.save(pago);
+                responseJson.put("id creado",pago.getId());
+                return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+            }else{
+                responseJson.put("error","El id del pago a crear ya existe");
+                return ResponseEntity.badRequest().body(responseJson);
+            }
+
+        }else{
+            responseJson.put("error","El id brindado debe ser mayor igual a 0 y diferente de null");
+            return ResponseEntity.badRequest().body(responseJson);
+        }
+        }
     //Exceptionhandlerpost
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<HashMap<String,String>> gestionException(HttpServletRequest request){
